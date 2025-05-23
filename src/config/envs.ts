4e0c42564de9f900172e79f4367e5dd31ff508d9
Jose Manuel Munoz Gutierrez <manuelinux@gmail.com>
2025-05-22 23:07:17 -0400

    Remove product service config and translate readme
    Removes unnecessary product microservice configuration from environment variables and related code. Updates development documentation by translating README from Spanish to English and improves development workflow by adding docker:start script to handle database migrations automatically during development.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,16 +3,12 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  PRODUCTS_MICRO_SERVICE_HOST: string;
-  PRODUCTS_MICRO_SERVICE_PORT: number;
   NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object({
     PORT: joi.number().required(),
-    PRODUCTS_MICRO_SERVICE_HOST: joi.string().required(),
-    PRODUCTS_MICRO_SERVICE_PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
@@ -34,7 +30,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   PORT: envVars.PORT,
-  productsMicroServiceHost: envVars.PRODUCTS_MICRO_SERVICE_HOST,
-  productsMicroServicePort: envVars.PRODUCTS_MICRO_SERVICE_PORT,
   natsServers: envVars.NATS_SERVERS,
 };
